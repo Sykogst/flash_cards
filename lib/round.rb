e57afa97ba3838ef3be466e8e4f0d35c5224a832
Sam Tran <samuelttran@gmail.com>
2023-08-23 15:47:45 -0600

    Add take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,24 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :card_index
   def initialize(deck)
     @deck = deck
     @turns = []
+    @card_index = 0
+    @question_card
+  end
+
+  def question_card
+      @question_card = @deck.cards[@card_index]
+  end
+
+  def take_turn(question_guess)
+    if @card_index == @deck.cards.length
+      'No more cards!'
+    else
+      turn_1 = Turn.new(question_guess, question_card)
+      @turns << turn_1
+      @card_index += 1
+      turn_1
+    end
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -25,6 +25,31 @@ RSpec.describe Round do
     it 'has turns' do
       expect(@round.turns).to eq([])
     end
+
+    it 'has question_card' do
+      expect(@round.question_card).to eq(@card_1)
+    end
+  end
+
+  describe '#take_turn' do
+    it 'take_turn creates Turn object, and moves to next card' do
+      new_turn = @round.take_turn('Juneau')
+      expect(new_turn).to eq(new_turn)
+      expect(new_turn.class).to eq(Turn)
+      expect(new_turn.correct?).to be true
+
+      another_turn = @round.take_turn('Bluegrass')
+      expect(another_turn).to eq(another_turn)
+      expect(another_turn.correct?).to be false
+    end
+
+    it 'no more cards left' do
+      new_turn = @round.take_turn('Juneau')
+      another_turn = @round.take_turn('Grass')
+      last_turn = @round.take_turn('Mars')
+      i_want_more = new_turn = @round.take_turn('MORE')
+      expect(i_want_more).to eq('No more cards!')
+    end
   end
 
 end
