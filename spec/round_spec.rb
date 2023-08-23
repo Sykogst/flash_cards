c1a8e753d1082537b5a1b34cee0c887da95f4caa
Sam Tran <samuelttran@gmail.com>
2023-08-23 13:28:29 -0600

    Add Round class and spec files

---
+++ spec/round_spec.rb
@@ -0,0 +1,26 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+  before(:each) do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("What is the name of the tallest grass?", "Bamboo", :STEM)
+    @card_3 = Card.new("What is the closest planet to the Sun?", "Mercury", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
+
+  describe '#initiailize' do
+    it 'Round exists' do
+      expect(@round).to be_instance_of(Round)
+    end
+
+    it 'has deck' do
+      expect(@round.deck).to eq(@deck)
+    end
+  end
+
+end
