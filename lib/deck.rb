3803d26a47fdcbb09497d1fd698023b5de6addd4
Sam Tran <samuelttran@gmail.com>
2023-08-23 20:22:28 -0600

    Refactored class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,18 @@
 class Deck
-  attr_reader :cards, :count, :category_deck
+  attr_reader :cards
   # cards, parameter containing array of card object instances from Card class
   def initialize(cards)
     @cards = cards
-    @count = cards.length
-    @category_deck = nil
+    @total_cards
   end
+
   # Method: cards_in_category, 1 param, returns cards that match the category
   def cards_in_category(category_filter)
-    @category_deck = @cards.select { |card| card.category == category_filter }
+    @cards.select { |card| card.category == category_filter }
   end
+
+  def total_cards
+    @cards.length
+  end
+
 end
